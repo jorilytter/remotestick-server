271c209fe39a03af916f10ef5707ad53c864addb
Jori Lytter <jori.lytter@gmail.com>
2015-10-19 20:53:12 +0300

    Added icon for health status and dummy implementation for listing devices

--- static/js/remote.js
+++ static/js/remote.js
@@ -1,3 +1,32 @@
+var devices = [{
+  id: "1",
+  name: "TV",
+  protocol: "foo",
+  model: "bar",
+  lastCommand: "ON",
+  lastValue: "tadaa",
+  supportedMethod: {id: "TELLSTICK_TURNOFF", name: "TELLSTICK_TURNOFF"}
+},{
+  id: "2",
+  name: "Työhuone",
+  protocol: "foo",
+  model: "bar",
+  lastCommand: "OFF",
+  lastValue: "tadaa",
+  supportedMethod: {id: "TELLSTICK_TURNON", name: "TELLSTICK_TURNON"}
+}]
+
+function deviceInfo(device) {
+  var container = document.getElementById('outlets-info')
+  var textC = document.createElement('p')
+  var text = document.createTextNode("id: " + device.id + "; name: " + device.name)
+  textC.appendChild(text)
+  container.appendChild(textC)
+}
+
+function listDevices() {
+  devices.map(deviceInfo)
+}
 
 function health() {
   var request = new XMLHttpRequest()
@@ -6,7 +35,20 @@ function health() {
   request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText)
-      document.getElementById('health-status').innerHTML = data.health
+      var statusText = document.createTextNode(data.health)
+      var icon = document.getElementById('health-status-icon')
+
+      if (data.health.toLowerCase() === 'ok') {
+        removeClass(icon, 'fa-heart-o')
+        removeClass(icon, 'gray')
+        addClass(icon, 'fa-heart')
+        addClass(icon, 'red')
+      } else {
+        removeClass(icon, 'fa-heart')
+        removeClass(icon, 'red')
+        addClass(icon, 'fa-heart-o')
+        addClass(icon, 'gray')
+      }
     } else {
       console.err('Error received from REST API', request.status, request.responseText)
     }
@@ -19,8 +61,25 @@ function health() {
   request.send()
 }
 
+function addClass(element, className) {
+  if (element.classList) {
+    element.classList.add(className)
+  } else {
+    element.className += ' ' + className
+  }
+}
+
+function removeClass(element, className) {
+  if (element.classList) {
+    element.classList.remove(className)
+  } else {
+    element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ')
+  }
+}
+
 document.onreadystatechange = function(){
   if (document.readyState === 'complete') {
     health()
+    listDevices()
   }
 }
